b36dc83e635d04c4babca6af632a4acba894f1dd
azantor <arifuzantor@gmail.com>
2021-06-20 19:33:58 +0600

    Update login.php

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if(isset($_SESSION["teacher_id"]))
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Student Attendance System in PHP using Ajax</title>
+  <title>SUST Attendance System</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
